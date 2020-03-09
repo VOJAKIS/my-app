888800da3bc280966073cc26b54fcb62bb7df0cf
VOJAKIS <50027263+VOJAKIS@users.noreply.github.com>
2020-03-09 18:20:18 +0100

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const url = require('url');
+
+// Helper URL method
+const iconUrl = url.format({
+  pathname: path.join(__dirname,'./icons/Icon.icns'),
+  protocol: 'file:',
+  slashes: true
+})
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -13,7 +21,8 @@ const createWindow = () => {
     height: 720,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    icon: (__dirname + './icons/Icons.icns') || iconUrl,
   });
 
   // and load the index.html of the app.
